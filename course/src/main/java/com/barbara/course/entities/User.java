b02892c33f30f2e8aedf7eee1f1a2113056db0d5
Barbara Camargo <bpcamargo9@hotmail.com>
2022-08-10 10:41:08 -0300

    fixing dependencies

--- course/src/main/java/com/barbara/course/entities/User.java
+++ course/src/main/java/com/barbara/course/entities/User.java
@@ -33,10 +33,6 @@ public class User implements Serializable {
         this.password = password;
     }
 
-    public static long getSerialversionuid() {
-        return serialVersionUID;
-    }
-
     public Long getId() {
         return id;
     }
