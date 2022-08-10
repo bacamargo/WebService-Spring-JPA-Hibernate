27c828597b2252ae74856d838544c606494ec66e
Barbara Camargo <bpcamargo9@hotmail.com>
2022-08-10 11:47:09 -0300

    Order, Instant, ISO 8601

--- course/src/main/java/com/barbara/course/entities/User.java
+++ course/src/main/java/com/barbara/course/entities/User.java
@@ -1,11 +1,14 @@
 package com.barbara.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -21,6 +24,9 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User() {
     }
@@ -71,6 +77,10 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     @Override
