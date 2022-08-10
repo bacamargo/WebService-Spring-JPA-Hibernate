c72d91ac9dd6e847301c32ee9daa43be1d9e319c
Barbara Camargo <bpcamargo9@hotmail.com>
2022-08-10 13:39:07 -0300

    Category

---
+++ course/src/main/java/com/barbara/course/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.barbara.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.barbara.course.entities.Category;
+import com.barbara.course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository repository;
+
+    public List<Category> findAll() {
+        return repository.findAll();
+    }
+
+    public Category findById(Long id) {
+        Optional<Category> obj = repository.findById(id);
+        return obj.get();
+    }
+}
